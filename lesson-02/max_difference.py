6b7375b3a96cd5b52b9c08d7c2dab60f57b2a82e
Chi-Ren Hwang <chiren.hwang@gmail.com>
2020-08-01 11:13:33 +0800

    Add max_difference

---
+++ lesson-02/max_difference.py
@@ -0,0 +1,18 @@
+
+# exercise:
+# 1. define the function
+
+def max_difference(a, b, c):
+    pass
+
+# 2. call you function below, ex: print(max_difference(7, -2, 3)), then save the file
+
+---
+
+# 3. Click Terminal => New Terminal to open a terminal window below
+
+# 4. In the termin window, type: python max_difference.py to run your code
+
+# 5. type: python -i max_difference.py to load your code in the python REPL
+
+# 6. Call your max_difference function in python REPL >>> max_difference(1, 2, 3)
