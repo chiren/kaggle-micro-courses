f544b877e28650a7a3e13851a2c06c7706473f36
Chi-Ren Hwang <chiren.hwang@gmail.com>
2020-08-01 15:28:42 +0800

    add inc

---
+++ python/lesson-02/inc.py
@@ -0,0 +1,14 @@
+
+# define a function call inc with 2 input values n and step with default value of 1
+# return the sum of n and step
+
+# define your function here
+
+
+
+# call your function without step(the second parameter)
+# something like print(inc(9))
+
+
+# call your function with step value
+# ex: print(inc(8, 9))
