b648c8764fcdbf9e4c2bbde5023794dae9ac3f18
Chi-Ren Hwang <chiren.hwang@gmail.com>
2020-08-08 11:46:15 +0800

    Add ex-09

---
+++ python/lesson-03/ex-09.py
@@ -0,0 +1,37 @@
+"""
+Exercise 09:
+
+Define a function named exactly_one_button_pressed which takes 2 argument b1, b2
+and return True only when exactly one button is pressed, otherwise return False
+
+Call your exactly_one_button_pressed function and print out the results
+
+*** Hint: use int() function to convert the arguments
+          to boolean values and add them up
+
+ex:
+
+a1, a2 = True, True
+a3, a4 = True, False
+a5, a6 = False, True
+a7, a8 = False, False
+
+print(a1, a2, "exactly one button pressed:", exactly_one_button_pressed(a1, a2))
+print(a3, a4, "exactly one button pressed:", exactly_one_button_pressed(a3, a4))
+print(a5, a6, "exactly one button pressed:", exactly_one_button_pressed(a5, a6))
+print(a7, a8, "exactly one button pressed:", exactly_one_button_pressed(a7, a8))
+
+
+"""
+
+
+# define your function here
+
+
+
+# Call your function and print out your results here
+
+
+
+
+
